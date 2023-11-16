27eccd8f12726d879f6946f871c49c8a4c014403
Lalitha <lalithaediga66@gmail.com>
2023-11-16 14:49:16 +0530

    first commit

---
+++ nz/urls.py
@@ -0,0 +1,6 @@
+from nz.views import *
+from django.urls import path
+app_name='will'
+urlpatterns=[
+    path('williamson/',williamson,name='williamson')
+]
---
+++ nz/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def williamson(request):
+    return HttpResponse('<h1>hope scores century</h1>')
